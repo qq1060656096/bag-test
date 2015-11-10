80607b9a206343b0bcc9e52180a101f447d9a4b3
unknown <1060656096@qq.com>
2015-11-10 23:57:50 +0800

    新增评论,完成18%

--- frontend/views/answer/step2_answer.php
+++ frontend/views/answer/step2_answer.php
@@ -189,10 +189,14 @@ function answer(){
 		<!-- template.cache.mobile-ceshi-show-questions.0c968bd61f3b7fa78b9483c21a4c8bea -->
 	</div>
 
-
+    <?php 
+        echo $this->renderFile(__DIR__ . '/../comment/static-list.php');
+    ?>
 </div>
 <!-- /page -->
 
-
+<?php 
+echo $this->renderFile(__DIR__ . '/../layouts/foot-comment.php');
+?>
 
 <?php echo $this->renderFile(__DIR__.'/../layouts/foot.php');?>
